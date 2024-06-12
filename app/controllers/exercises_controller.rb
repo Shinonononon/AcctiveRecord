cefab363bb50cf93a762be15dd424b5f1ca68a59
Shinonononon <h1karu.sn0928@gmail.com>
2024-06-12 10:24:14 +0900

    test

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -16,8 +16,11 @@ class ExercisesController < ApplicationController
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address.joins(:orders).select('addresses.*, COUNT(orders.id) as orders_count').group('addresses.id').order('orders_count DESC').first
-    @address.orders_count  # これで注文の数を返す
+    @address = Address.joins(:orders)
+    .select('addresses.*, COUNT(orders.id) as orders_count')
+    .group('addresses.id')
+    .order('orders_count DESC')
+    .first
 
   end
 
@@ -25,8 +28,11 @@ class ExercisesController < ApplicationController
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer.joins(orders: { order_foods: :food }).select('customers.*, SUM(foods.price) as foods_price_sum').group('customers.id').order('foods_price_sum DESC').first
-    @customer.foods_price_sum  # これで合計金額を返す
+    @customer = Customer.joins(orders: { order_foods: :food })
+    .select('customers.*, SUM(foods.price) as foods_price_sum')
+    .group('customers.id')
+    .order('foods_price_sum DESC')
+    .first
 
   end
 end
